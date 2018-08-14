b01350c0b24d4de400915c8b5e29b6089503cdc2
Kieran Chadwick <kieran.chadwick@outlook.com>
2018-08-14 16:42:51 +0100

    countdown test tweaks

--- src/components/__tests__/Countdown.test.js
+++ src/components/__tests__/Countdown.test.js
@@ -2,29 +2,13 @@
 // should take date to countdown to
 
 import React from 'react';
-import { Text } from 'react-native';
 import { shallow } from 'enzyme';
 
 import Countdown from '../Countdown';
 
-const component = <Countdown title="Next Race" />;
+const component = <Countdown title="Next Race" countdownTo={new Date()} />;
 
 it('renders correctly', () => {
     const wrapper = shallow(component);
     expect(wrapper).toMatchSnapshot();
-});
-
-it('displays the title', () => {
-    const clone = React.cloneElement(component);
-    const wrapper = shallow(clone);
-    expect(wrapper.find(Text).contains('Next Race')).toBeTruthy();
-});
-
-it('shows the countdown in the right format', () => {
-    const clone = React.cloneElement(component);
-    const wrapper = shallow(clone).render();
-
-    console.log(wrapper);
-    //console.log(wrapper.find(Text).last().);
-    //expect(wrapper.find(Text).last().text).toBeTruthy();
 });
